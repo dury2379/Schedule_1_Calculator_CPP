7fe7c46abeef5693da93a60873052cc00fcd31e1
den <dennis.uryeff@gmail.com>
2026-04-19 02:10:53 -0700

    Partially finished writing comments for "Products.h".

--- Products/Product.h
+++ Products/Product.h
@@ -16,21 +16,37 @@
 
 class Product {
 private:
+	// Original, "raw", product.
 	Products_Lib::starter_product root_prod;
+	// List of effects. It is in format of a 64-bit concatenated int.
 	uint64_t effects;
+	// Chain of ingredients that lead to this product, starting at root_prod.
 	std::vector<Ingredients_Lib::ingredient_type> Ingredients_Chain;
+	// This function is used to create a tabbed list of effects. Only used by to_string method.
 	std::string effects_to_string(std::string&) const;
+	// This function is used to create a tabbed indexed list of ingredients. Only used by to_string method.
 	std::string ingredients_to_string(std::string&);
 public:
+	// Classic constructor. Initializes all class members.
 	Product(Products_Lib::starter_product, uint64_t, std::vector<Ingredients_Lib::ingredient_type>);
+	// A copy constructor.
 	Product(Product*);
+	// Mixes current product with the ingredient. Generates new Product.
+	// IS NOT RESPONSIBLE FOR NEW PRODUCT'S MEMORY MANAGEMENT!!!
 	Product* mix(Ingredients_Lib::ingredient_type);
+	// Goes through all ingredients and adds up their costs.
 	int get_ingredients_cost();
+	// Calculates the normal price (street value) of the product.
 	int get_total_price() const;
+	// Generates a nice looking print out string for the product.
 	std::string to_string();
+	// Generates a print out string for the product with std::string prefix attached.
 	std::string to_string(std::string);
+	// Returns effects list in 64-bit concatenated int format.
 	uint64_t get_effects();
+	// Length of ingredients chain.
 	int get_ingredients_chain_depth();
+	//
 	bool ingredients_chain_equals(const std::vector<Ingredients_Lib::ingredient_type>&) const;
 	int get_profit();
 	bool contains(const Product*) const;
