648c45c85c85043caf06c3e1b277a52e46a27f69
den <dennis.uryeff@gmail.com>
2026-04-24 01:36:02 -0700

    Added comments to "Ingredients_Lib.h".

--- Ingredients/Ingredients_Lib.h
+++ Ingredients/Ingredients_Lib.h
@@ -12,6 +12,9 @@
 
 namespace Ingredients_Lib
 {
+	/*
+	 * Enum all ingredients.
+	 */
 	enum class ingredient_type
 	{
 		ADDY,
@@ -32,6 +35,9 @@ namespace Ingredients_Lib
 		VIAGRA
 	};
 
+	/*
+	 * A vector of all ingredients. Used in BFS.
+	 */
 	static const std::vector<ingredient_type> ingredients_vector = {
 		ingredient_type::ADDY,
 		ingredient_type::BANANA,
@@ -50,6 +56,9 @@ namespace Ingredients_Lib
 		ingredient_type::VIAGRA
 	};
 
+	/*
+	 * Map where keys are ingredients, and values are base effects.
+	 */
 	static const std::map<ingredient_type, Effects_Lib::effect_enum> ingredient_base_effect = {
 		{ingredient_type::ADDY, Effects_Lib::effect_enum::THOUGHT_PROVOKING},
 		{ingredient_type::BANANA, Effects_Lib::effect_enum::GINGERITIS},
@@ -69,6 +78,9 @@ namespace Ingredients_Lib
 		{ingredient_type::VIAGRA, Effects_Lib::effect_enum::TROPIC_THUNDER}
 	};
 
+	/*
+	 * Map where keys are ingredients, and values are cost. Used for product cost calculation.
+	 */
 	static const std::map<ingredient_type, int> ingredients_costs = {
 		{ingredient_type::ADDY, 9},
 		{ingredient_type::BANANA, 2},
@@ -88,6 +100,9 @@ namespace Ingredients_Lib
 		{ingredient_type::VIAGRA, 4}
 	};
 
+	/*
+	 * Map where keys are ingredients, and values are string with their names. Used for products to_string method.
+	 */
 	static const std::map<ingredient_type, std::string> ingredient_to_string = {
 		{ingredient_type::ADDY, "Addy"},
 		{ingredient_type::BANANA, "Banana"},
@@ -111,6 +126,17 @@ namespace Ingredients_Lib
 
 				{Effects_Lib::effect_enum::, {Effects_Lib::effect_enum::, Effects_Lib::effect_enum::}},
 	*/
+
+	/*
+	 * Map that is used for mixing.
+	 * A -- the name of the effect that is going to be removed.
+	 * B -- the name of the effect that is going to be added to replace the removed effect A.
+	 * I -- name of the ingredient to which these transition pairs apply.
+	 * The map is structured as such: Maps keys are I, and values are another map that holds all pairs of As and
+	 * corresponding Bs. Inside the nested map it is structured like so: keys are the As, and values are pairs of A, B.
+	 * Using pairs becomes important if a product already has the B effect, and the transition has to be canceled and
+	 * effect A must be put back.
+	 */
 	static const std::map<ingredient_type, std::map<Effects_Lib::effect_enum, std::pair<Effects_Lib::effect_enum, Effects_Lib::effect_enum>>> transforms_map = {
 		{
 			ingredient_type::ADDY, {
@@ -292,32 +318,53 @@ namespace Ingredients_Lib
 		}
 	};
 
+	/*
+	 * This function takes in concatenated 64-bit int (effects list) and an ingredient and produces mix (in concatenated
+	 * 64-bit int format).
+	 */
 	inline std::uint64_t process_effects(ingredient_type ingr, const std::uint64_t effects_int64)
 	{
+		// First initialize new_effects_vector with old effects.
 		std::vector<Effects_Lib::effect_enum> new_effects_vector = Effects_Lib::to_effects_vector(effects_int64);
+		// Initialize staged transform vector, a vector of <A, B> pairs (see explanation for transforms_map).
 		std::vector<std::pair<Effects_Lib::effect_enum, Effects_Lib::effect_enum>> staged_transforms = {};
 
+		// Go through all the new effects.
 		for (int i = new_effects_vector.size() - 1; i >= 0; i--)
 		{
+			// If transforms_map has the effect at index i then:
 			if (transforms_map.at(ingr).contains(new_effects_vector[i]))
 			{
+				// Add corresponding <A, B> pair to the staged_transforms vector.
 				staged_transforms.push_back(transforms_map.at(ingr).at(new_effects_vector[i]));
+				// Remove the effect at index i (effect A).
 				new_effects_vector.erase(new_effects_vector.begin() + i);
 			}
 		}
 
+		// Go through all <A, B> pairs.
 		for (std::pair<Effects_Lib::effect_enum, Effects_Lib::effect_enum>& transform: staged_transforms)
 		{
+			// If B already exist in new_effects_vector then:
 			if (std::find(new_effects_vector.begin(), new_effects_vector.end(), transform.second) != new_effects_vector.end())
 			{
+				// Reverse the <A, B> pair. New pair becomes <B, A> pair.
 				std::swap(transform.first, transform.second);
 			}
+			// Add the second part of each pair. If B is not a part of new_effects_vector then B is added.
+			// If B is already a part of new_effects_vector, then the pair will be reversed by the if statement above,
+			// so the second part of the reverse pair is A. This effectively cancels the effect transition
 			new_effects_vector.push_back(transform.second);
 		}
 
+		// This section add a base effect. The max number of effects per product is 8, so if there is more than 8
+		// effects then the base effect should not be added.
+		// Also, if the base effects is already present then it should not be added a second time.
+		// This if statement checks for the conditions described above.
 		if (new_effects_vector.size() < 8 &&
 			std::find(new_effects_vector.begin(), new_effects_vector.end(), ingredient_base_effect.at(ingr)) == new_effects_vector.end())
 		{
+			// If condition above is met, add the base effect to this new_effects_vector.
 			new_effects_vector.push_back(ingredient_base_effect.at(ingr));
 		}
 
